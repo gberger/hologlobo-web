d34811c456c378487fb7a94b0d2bdc80ea98dbf3
Guilherme Berger <guilherme.berger@gmail.com>
2015-09-05 15:20:52 -0300

    simplify mongodb config

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
+require('dotenv').load();
 var express = require('express');
 var cfenv = require('cfenv');
 var _ = require('lodash');
+var mongodb = require('mongodb')
 
+var MongoClient = mongodb.MongoClient;
 var appEnv = cfenv.getAppEnv();
 
-var environment = process.env.NODE_ENV;
-if (appEnv.isLocal) {
-  var envVars = require('./env.json');
-  process.env.MONGODB_URI = envVars.VCAP_SERVICES['user-provided'][0].credentials.uri;
-  process.env.MONGODB_PORT = envVars.VCAP_SERVICES['user-provided'][0].credentials.port;
-  process.env.MONGODB_USER = envVars.VCAP_SERVICES['user-provided'][0].credentials.user;
-  process.env.MONGODB_PASSWORD = envVars.VCAP_SERVICES['user-provided'][0].credentials.password;
-} else {
-  process.env.MONGODB_URI = appEnv.VCAP_SERVICES['user-provided'][0].credentials.uri;
-  process.env.MONGODB_PORT = appEnv.VCAP_SERVICES['user-provided'][0].credentials.port;
-  process.env.MONGODB_USER = appEnv.VCAP_SERVICES['user-provided'][0].credentials.user;
-  process.env.MONGODB_PASSWORD = appEnv.VCAP_SERVICES['user-provided'][0].credentials.password;
-}
+MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
+  if (err) console.log(err);
+
+  var collection = db.collection('test');
+});
+
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.listen(appEnv.port, function() {
+app.listen(process.env.PORT || appEnv.port, function() {
   console.log("server starting on " + appEnv.url);
-  console.log(process.env.MONGODB_USER);
 });
